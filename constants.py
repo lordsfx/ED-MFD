af6b669a703cbe9a91d78047d20fae51ec733c21
Chris Chan <chris@chan.de.com>
2017-12-23 18:46:40 +0800

    moving constants to separat source

--- constants.py
+++ constants.py
@@ -1,4 +1,34 @@
-# color
+# Colors
 COLOR_ORANGE = (255, 153,  51)
 COLOR_GREEN  = ( 51, 255,  51)
 COLOR_GREY   = (128, 128, 128)
+
+# MFD Layout
+#     X L C1 C2 C3 C4 C5 R       Y
+# |28|    01 02 03 04 05    |21| T
+# |27| 20                06 |22| C1
+#      19                07      C2
+#      18                08      C3
+#      17                09      C4
+# |26| 16                10 |23| C5
+# |25|    15 14 13 12 11    |24| B
+
+# Positions
+MFD_XL1 = 2
+MFD_XC1 = 100
+MFD_XC2 = 224
+MFD_XC3 = 346
+MFD_XC4 = 464
+MFD_XC5 = 582
+MFD_XR1 = 676
+MFD_YC1 = 158
+MFD_YC2 = 270
+MFD_YC3 = 382
+MFD_YC4 = 490
+MFD_YC5 = 600
+MFD_YT1 = 12
+MFD_YB1 = 730
+MFD_RP_X = 906
+MFD_RP_Y = 0
+MFD_RP_WIDTH = 380
+MFD_RP_HEIGHT = 800
--- mfd.py
+++ mfd.py
@@ -35,35 +35,6 @@ img_MFD.blit(layer_BTN, (0, 0))
 BTN1_SIZE = BTN1_WIDTH, BTN1_HEIGHT = scaled(112), scaled(50)
 TIMER_LOOP = Button.TIMER_STEP * 10
 
-# MFD Layout
-#     X L C1 C2 C3 C4 C5 R       Y
-# |28|    01 02 03 04 05    |21| T
-# |27| 20                06 |22| C1
-#      19                07      C2
-#      18                08      C3
-#      17                09      C4
-# |26| 16                10 |23| C5
-# |25|    15 14 13 12 11    |24| B
-
-# Positions
-MFD_XL1 = 2
-MFD_XC1 = 100
-MFD_XC2 = 224
-MFD_XC3 = 346
-MFD_XC4 = 464
-MFD_XC5 = 582
-MFD_XR1 = 676
-MFD_YC1 = 158
-MFD_YC2 = 270
-MFD_YC3 = 382
-MFD_YC4 = 490
-MFD_YC5 = 600
-MFD_YT1 = 12
-MFD_YB1 = 730
-MFD_RP_X = 900
-MFD_RP_Y = 0
-MFD_RP_WIDTH = 380
-MFD_RP_HEIGHT = 800
 MFD_RP_SIZE = scaled(MFD_RP_WIDTH), scaled(MFD_RP_HEIGHT)
 MFD_RP_XY = scaled(MFD_RP_X), scaled(MFD_RP_Y)
 
